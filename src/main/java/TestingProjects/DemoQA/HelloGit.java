5a424d870b85bf3db7eba95ab6bf55fb2936245d
Praveen <praveen.biocomparch@gmail.com>
2025-07-17 16:34:35 +0530

    HelloGit version 3

--- src/main/java/TestingProjects/DemoQA/HelloGit.java
+++ src/main/java/TestingProjects/DemoQA/HelloGit.java
@@ -2,7 +2,7 @@ package TestingProjects.DemoQA;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("2 Hello from Git and GitHub!");
+        System.out.println("3333333");
     }
 }
 
